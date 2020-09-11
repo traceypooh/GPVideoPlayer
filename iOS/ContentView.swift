507765962c146f028a234a45f2101b598c652d02
tracey jaquith <tracey_pooh@yahoo.com>
2020-09-11 00:20:34 -0700

    iOS and tvOS targets cleaned up.  (basic) app icons added

---
+++ iOS/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  iOS
+//
+//  Created by tracey on 9/10/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
